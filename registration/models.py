be4f4886255e48ec7c2358c62877c340f5966fd7
Sai-Adhinatha-Reddy <subbugona@gmail.com>
2025-04-01 21:48:25 -0500

    Added new field email

--- registration/models.py
+++ registration/models.py
@@ -7,6 +7,8 @@ class Registration(models.Model):
     father_name = models.CharField(max_length=255)
     mobile_number = models.CharField(max_length=20)
     location = models.CharField(max_length=255)
+    email_validator = RegexValidator(r'^[^@]+@[^@]+\.[^@]+$', 'Enter a valid email address with "@" symbol.')
+        email = models.CharField(max_length=255, validators=[email_validator])
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
