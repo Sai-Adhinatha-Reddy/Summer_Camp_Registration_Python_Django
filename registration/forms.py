fbe46af1b7233cf90376f3ee685b9d0128e94218
Sai-Adhinatha-Reddy <91363558+Sai-Adhinatha-Reddy@users.noreply.github.com>
2025-04-04 20:45:44 -0500

    Update forms.py

--- registration/forms.py
+++ registration/forms.py
@@ -4,4 +4,4 @@ from .models import Registration
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Registration
-        fields = ['full_name', 'age', 'gender', 'father_name', 'mobile_number', 'location']
+        fields = ['full_name', 'age', 'gender', 'father_name', 'mobile_number', 'location', 'email']
